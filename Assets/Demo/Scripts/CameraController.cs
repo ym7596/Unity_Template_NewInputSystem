a480ab0a15ac4cd25675d0e1977d5de34130ec97
YONGMIN <ym7596@naver.com>
2024-02-08 18:05:53 +0900

    feat : SampleScene Update

--- Assets/Demo/Scripts/CameraController.cs
+++ Assets/Demo/Scripts/CameraController.cs
@@ -1,23 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraController
 {
-    private InputManager inputs;
-    private Transform camTransform;
+    private InputManager _inputs;
+    private Transform _camTransform;
     
-    private float maxSpeed = 10f;
+    private float _maxSpeed = 10f;
 
-    private float speed;
+    private float _speed;
 
-    private float accelration = 10f;
+    private float _accelration = 10f;
 
     private float _damping = 15f;
     
     //rotation
-    private float normalizedConst = .1f;
+    private float _normalizedConst = .1f;
 
     private float _dragSpeed = 2f;
     //screen edge motion
@@ -34,25 +32,27 @@ public class CameraController
     private Vector3 _startDrag = Vector3.zero;
    
     private Vector2 _movementValue;
+    private Vector3 _movementValueVertical;
 
     public CameraController(Transform cam, InputManager inputManager)
     {
-        camTransform = cam;
-        inputs = inputManager;
+        _camTransform = cam;
+        _inputs = inputManager;
         Init();
     }
     
     private void Init()
     {
-        inputs.OnActionWASD += SetWASD;
-        inputs.OnActionScrollWheel += SetWheelScroll;
-        inputs.OnRightClickedReleased += DragClear;
-        inputs.OnKeyboardListener += GetKeysValue;
+        _inputs.OnActionWASD += SetWASD;
+        _inputs.OnActionScrollWheel += SetWheelScroll;
+        _inputs.OnRightClickedReleased += DragClear;
+        _inputs.OnKeyboardListener += GetKeysValue;
     }
 
     public void Update()
     {
         GetKeyBoardMovement(); //WASD버튼 이동
+        GetKeyBoardMovementUp(); //QE 이동
         GetScrollButtonMovement(); //스크롤버튼 드래그 이동
         SetRotateByRightButton(); // 우클릭 회전
       
@@ -69,48 +69,58 @@ public class CameraController
         float val = value / 120;
         Vector3 wheelValue = Vector3.forward * val;
         
-        camTransform.Translate(wheelValue,Space.Self);
+        _camTransform.Translate(wheelValue,Space.Self);
     }
     private void SetWASD(Vector2 dir)
     {
         _movementValue = dir;
     }
+
+    private void SetQE(bool isUp)
+    {
+        if (isUp)
+            _movementValueVertical = Vector3.up;
+        else
+        {
+            _movementValueVertical = Vector3.down;
+        }
+    }
     
     private void SetRotateByRightButton()
     {
-        if (inputs.RightIsClicked == false || inputs.ScrollIsClicked == true)
+        if (_inputs.RightIsClicked == false || _inputs.ScrollIsClicked == true)
             return;
         if(_startDrag == Vector3.zero)
-            _startDrag = inputs.PrimaryRightPos;
-        Vector3 currentPos = inputs.Pos;
+            _startDrag = _inputs.PrimaryRightPos;
+        Vector3 currentPos = _inputs.Pos;
 
         Vector3 dragDelta = currentPos - _startDrag;
 
-        float rotX = -dragDelta.y * normalizedConst;
-        float rotY = dragDelta.x * normalizedConst;
+        float rotX = -dragDelta.y * _normalizedConst;
+        float rotY = dragDelta.x * _normalizedConst;
 
        
-        camTransform.Rotate(Vector3.up, rotY, Space.Self); // y축 회전
-        camTransform.Rotate(Vector3.right, rotX, Space.Self); 
+        _camTransform.Rotate(Vector3.up, rotY, Space.Self); // y축 회전
+        _camTransform.Rotate(Vector3.right, rotX, Space.Self); 
       
-      float z = camTransform.eulerAngles.z;
-       camTransform.Rotate(0,0,-z);
+      float z = _camTransform.eulerAngles.z;
+       _camTransform.Rotate(0,0,-z);
         _startDrag = currentPos;
     }
 
     private void GetScrollButtonMovement()
     {
-        if (inputs.ScrollIsClicked == false || inputs.RightIsClicked == true)
+        if (_inputs.ScrollIsClicked == false || _inputs.RightIsClicked == true)
             return;
 
         if (_startDrag == Vector3.zero)
-            _startDrag = inputs.PrimaryWheelPos;
-        Vector3 currentPos = inputs.Pos;
+            _startDrag = _inputs.PrimaryWheelPos;
+        Vector3 currentPos = _inputs.Pos;
         Vector3 dragDelta = currentPos - _startDrag;
 
-        dragDelta = dragDelta.normalized * normalizedConst;
+        dragDelta = dragDelta.normalized * _normalizedConst;
         Vector3 moveDir = new Vector3(-dragDelta.x, -dragDelta.y, 0) * _dragSpeed;
-        camTransform.Translate(moveDir,Space.Self);
+        _camTransform.Translate(moveDir,Space.Self);
         //좌우 X축 -+  상하 Y축 +-
         
         _startDrag = currentPos;
@@ -128,8 +138,10 @@ public class CameraController
             switch (keyName)
             {
                 case "Q":
+                    SetQE(true);
                     break;
                 case "E":
+                   SetQE(false);
                     break;
                 case "Space":
                     break;
@@ -139,17 +151,33 @@ public class CameraController
                     break;
             }
         }
+        else if (phase == InputActionPhase.Canceled)
+        {
+            switch (keyName)
+            {
+                case "Q":
+                case "E":
+                    _movementValueVertical = Vector3.zero;
+                    break; 
+            }
+        }
     }
 
-#region WASD MOVE
+#region WASDQE MOVE
+
+    private void GetKeyBoardMovementUp()
+    {
+        _speed = Mathf.Lerp(_speed, _maxSpeed, Time.deltaTime * _accelration);
+        _camTransform.Translate(_movementValueVertical *_speed* Time.deltaTime,Space.World);
+    }
     private void GetKeyBoardMovement()
     {
-        Vector3 inputValue = _movementValue.x * GetCameraRight(camTransform)
-                             + _movementValue.y * GetCameraForward(camTransform);
+        Vector3 inputValue = _movementValue.x * GetCameraRight(_camTransform)
+                             + _movementValue.y * GetCameraForward(_camTransform);
       
         inputValue = inputValue.normalized;
 
-        if (inputValue.sqrMagnitude > 0.1f)
+        if (inputValue.sqrMagnitude > 0.1f) 
             _targetPosition += inputValue;
     }
     
@@ -157,13 +185,13 @@ public class CameraController
     {
         if (_targetPosition.sqrMagnitude > 0.1f)
         {
-            speed = Mathf.Lerp(speed, maxSpeed, Time.deltaTime * accelration);
-            camTransform.position += _targetPosition * speed * Time.deltaTime;
+            _speed = Mathf.Lerp(_speed, _maxSpeed, Time.deltaTime * _accelration);
+            _camTransform.position += _targetPosition * _speed * Time.deltaTime;
         }
         else
         {
             _horizontalVelocity = Vector3.Lerp(_horizontalVelocity, Vector3.zero, Time.deltaTime * _damping);
-            camTransform.position += _horizontalVelocity * Time.deltaTime;
+            _camTransform.position += _horizontalVelocity * Time.deltaTime;
         }
 
         _targetPosition = Vector3.zero;
@@ -178,7 +206,12 @@ public class CameraController
 
     private Vector3 GetCameraForward(Transform transform)
     {
-        return transform.transform.TransformDirection(Vector3.forward);
+        return transform.TransformDirection(Vector3.forward);
+    }
+
+    private Vector3 GetCameraUp(Transform transform)
+    {
+        return transform.TransformDirection(Vector3.up);
     }
 #endregion
 
--- Assets/Demo/Scripts/Test.cs
+++ Assets/Demo/Scripts/Test.cs
@@ -1,20 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-   [SerializeField]  private InputManager _inputManager;
+    [SerializeField]  private InputManager _inputManager;
+    [SerializeField] private Transform _camTransform;
 
+    private CameraController _camController;
     private TouchPhases phase;
     // Start is called before the first frame update
    
     private bool begin = false;
+
+    private void Start()
+    {
+        _camController = new CameraController(_camTransform,_inputManager);
+    }
+
     // Update is called once per frame
     void Update()
     {
        // phase = _inputManager.CurrentPhase;
-       
+       _camController.Update();
         switch (_inputManager.CurrentPhase)
         {
             case TouchPhases.None:
